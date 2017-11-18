aa38870261d34b8b35a4203b0aa61a38725d039d
Heather <hlmathies@gmail.com>
2017-11-18 16:37:37 -0500

    Add server.js

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var db = require(".models");
 
 var PORT = process.env.PORT || 3000;
 var app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public"));
 //this guarantees our server won't start before our database is ready or if
 //there is an error
-db.sequelize.sync().then(function(){
+// Routes
+// =============================================================
+//html routes
+require("./routes/addRoom.js")(app);
+require("./routes/landing.js")(app);
+require("./routes/register")(app);
+require("./routes/vacantRooms")(app);
+//api routes
+require("./controllers/hotel.js")(app);
+require("./controllers/room.js")(app);
+
+db.sequelize.sync({force: true }).then(function(){
   //syncing our database.  creates tables using the models described
   app.listen(PORT, function(){
     console.log("Listening on port %s", PORT);
