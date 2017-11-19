a69e334a62d16a51c6736c09a215b867ff5f8a48
Heather <hlmathies@gmail.com>
2017-11-18 20:35:25 -0500

    Alter html routes to index.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,14 +12,14 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // landing route loads the welcome page and contains login and registration
+  // index.html loads the welcome page and contains login and registration
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/welcome.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
   });
 
-  // rooms route is the page where users can either add a room or explore vacant rooms
-  app.get("/rooms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/rooms.html"));
+  // this route is the page where users can either add a room or explore vacant rooms
+  app.get("/<file_name>", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/assets/<file_name>.html"));
   });
 
 
