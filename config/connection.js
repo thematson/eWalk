93202540af145f151ca34fd494dfec6fe2dffa5c
Heather <hlmathies@gmail.com>
2017-11-21 19:57:30 -0500

    change connection.js to add jawsDB config

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,48 @@
 // *********************************************************************************
 //
 // Dependencies
-var Sequelize = require("sequelize");
-var config = require('./config/config.json');
+// var Sequelize = require("sequelize");
+// var config = require('./config/config.json');
+//
+// // Creates mySQL connection using Sequelize
+// var sequelize = new Sequelize(config.development.database, "root", "password", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-// Creates mySQL connection using Sequelize
-var sequelize = new Sequelize(config.development.database, "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
+// Pull in required dependencies
+var mysql = require('mysql2');
+
+// Create the MySQL connection object
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'ewalk'
+	})
+};
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
   }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+// Export connection for ORM use
+module.exports = connection;
