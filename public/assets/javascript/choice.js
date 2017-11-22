8a2272b58b52111eeb4f1898bf4fba68802f96e8
Eric Matson <matson@Erics-MacBook-Pro.local>
2017-11-21 21:18:59 -0500

    added more html and js pages

---
+++ public/assets/javascript/choice.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $("#searchLogo").click(function() {
+      location.href = "./searchRooms.html";
+    });
+
+    $("#postLogo").click(function () {
+        location.href = "./postRooms.html";
+        
+    });
+     
+});
