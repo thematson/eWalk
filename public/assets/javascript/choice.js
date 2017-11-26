bc9b99aa26d2b428bb428f58a7b015183f9f8233
Heather <hlmathies@gmail.com>
2017-11-25 20:50:20 -0500

    trying to fix merge conflict in choice.js

--- public/assets/javascript/choice.js
+++ public/assets/javascript/choice.js
@@ -2,19 +2,12 @@ $(document).ready(function() {
 
     $("#searchLogo").click(function() {
 <<<<<<< HEAD
-      location.href = "./browseRooms.html";
-    });
-
-    $("#postLogo").click(function () {
-        location.href = "./postRooms.html";
-
-=======
       location.href = "/browseRooms";
     });
 
     $("#postLogo").click(function () {
         location.href = "/postRooms";
-        
+
 >>>>>>> e839e4998e162872f38b1b92fb6dc0c1af6b33ad
     });
 
