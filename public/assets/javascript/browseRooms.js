dd7e180c4a91a3dbd0ae1ab4c10a79a8784af293
Misael Rivera <mrivera0709@gmail.com>
2017-11-29 11:25:37 -0500

    improved UI and Media Queries

--- public/assets/javascript/browseRooms.js
+++ public/assets/javascript/browseRooms.js
@@ -1,78 +1,120 @@
 $(document).ready(function() {
  
-  // Getting the intiial list of Authors
-  getRooms();
+  // searchContainer holds all of our posts
+  var searchContainer = $(".searchContainer");
+  var postZipCodeSelect = $("#zipSubmit");
 
- 
+  // =======================================================
+  //IN PROGRESS -->  CHOOSE ZIP CODE
 
-  // Function for creating a new list row for authors
-  function createAuthorRow(authorData) {
-    console.log(authorData);
-    var newTr = $("<tr>");
-    newTr.data("author", authorData);
-    newTr.append("<td>" + authorData.name + "</td>");
-    newTr.append(
-      "<td># of posts will display when we learn joins in the next activity!</td>"
-    );
-    newTr.append(
-      "<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>"
-    );
-    newTr.append(
-      "<td><a href='/cms?author_id=" +
-        authorData.id +
-        "'>Create a Post</a></td>"
-    );
-    newTr.append(
-      "<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>"
-    );
-    return newTr;
+
+
+
+  // =======================================================
+  //IN PROGRESS -->  BOOKING ROOM
+  $(document).on("click", "#yes", bookRoom);
+
+  function bookRoom() {
+    var currentPost = $(this)
+      .parent()
+      .parent()
+      .data("post");
+    bookingPost(currentPost.id);
   }
 
-  // Function for retrieving authors and getting them ready to be rendered to the page
-  function getAuthors() {
-    $.get("/api/authors", function(data) {
-      var rowsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(createAuthorRow(data[i]));
-      }
-      renderAuthorList(rowsToAdd);
-      nameInput.val("");
-    });
-  }
-
-  // A function for rendering the list of authors to the page
-  function renderAuthorList(rows) {
-    authorList
-      .children()
-      .not(":last")
-      .remove();
-    authorContainer.children(".alert").remove();
-    if (rows.length) {
-      console.log(rows);
-      authorList.prepend(rows);
-    } else {
-      renderEmpty();
-    }
-  }
-
-  // Function for handling what to render when there are no authors
-  function renderEmpty() {
-    var alertDiv = $("<div>");
-    alertDiv.addClass("alert alert-danger");
-    alertDiv.text("You must create an Author before you can create a Post.");
-    authorContainer.append(alertDiv);
-  }
-
-  // Function for handling what happens when the delete button is pressed
-  function handleDeleteButtonPress() {
-    var listItemData = $(this)
-      .parent("td")
-      .parent("tr")
-      .data("author");
-    var id = listItemData.id;
+  function bookingPost(id) {
     $.ajax({
       method: "DELETE",
-      url: "/api/authors/" + id
-    }).done(getAuthors);
+      url: "/api/posts/" + id
+    })
+    .done(function() {
+      getPosts(postCategorySelect.val());
+    });
+  };
+// =======================================================
+
+getRooms();
+
+// This function grabs posts from the database and updates the view
+function getRooms(category) {
+  var categoryString = category || "";
+  if (categoryString) {
+    categoryString = "/category/" + categoryString;
   }
-});
+  $.get("/api/posts" + categoryString, function(data) {
+    console.log("Posts", data);
+    posts = data;
+    if (!posts || !posts.length) {
+      displayEmpty();
+    }
+    else {
+      initializeRows();
+    }
+  });
+}
+
+function initializeRows() {
+  blogContainer.empty();
+  var postsToAdd = [];
+  for (var i = 0; i < posts.length; i++) {
+    postsToAdd.push(createNewRow(posts[i]));
+  }
+  blogContainer.append(postsToAdd);
+}
+
+// This function constructs a post's HTML
+function createNewRow(post) {
+  var newPostPanel = $("<div>");
+  newPostPanel.addClass("panel panel-default");
+  var newPostPanelHeading = $("<div>");
+  newPostPanelHeading.addClass("panel-heading");
+  var deleteBtn = $("<button>");
+  deleteBtn.text("x");
+  deleteBtn.addClass("delete btn btn-danger");
+  var editBtn = $("<button>");
+  editBtn.text("EDIT");
+  editBtn.addClass("edit btn btn-default");
+  var newPostTitle = $("<h2>");
+  var newPostDate = $("<small>");
+  var newPostCategory = $("<h5>");
+  newPostCategory.text(post.category);
+  newPostCategory.css({
+    float: "right",
+    "font-weight": "700",
+    "margin-top":
+    "-15px"
+  });
+  var newPostPanelBody = $("<div>");
+  newPostPanelBody.addClass("panel-body");
+  var newPostBody = $("<p>");
+  newPostTitle.text(post.title + " ");
+  newPostBody.text(post.body);
+  var formattedDate = new Date(post.createdAt);
+  formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+  newPostDate.text(formattedDate);
+  newPostTitle.append(newPostDate);
+  newPostPanelHeading.append(deleteBtn);
+  newPostPanelHeading.append(editBtn);
+  newPostPanelHeading.append(newPostTitle);
+  newPostPanelHeading.append(newPostCategory);
+  newPostPanelBody.append(newPostBody);
+  newPostPanel.append(newPostPanelHeading);
+  newPostPanel.append(newPostPanelBody);
+  newPostPanel.data("post", post);
+  return newPostPanel;
+}
+
+  // This function displays a messgae when there are no posts
+  function displayEmpty() {
+    blogContainer.empty();
+    var messageh2 = $("<h2>");
+    messageh2.css({ "text-align": "center", "margin-top": "50px" });
+    messageh2.html("No posts yet for this category, navigate <a href='/cms'>here</a> in order to create a new post.");
+    blogContainer.append(messageh2);
+  }
+
+
+
+
+
+})
