aaed9eabcc6438e215c0b7c58a7a8a8e5eb9cf82
Heather <hlmathies@gmail.com>
2017-12-03 15:12:50 -0500

    forget this lonely file in clean up commit

--- public/assets/javascript/choice.js
+++ public/assets/javascript/choice.js
@@ -1,7 +1,7 @@
 // *********************************************************************************
 // CHOICE.JS - ON CLICK FUNCTIONS WHICH DIRECT USERS TO EITHER POST A ROOM OR SEARCH FOR ROOMS
 // *********************************************************************************
-//~functions for choice.html where users choice to either search or post vacant hotel rooms
+
 $(document).ready(function() {
   var parts = this.URL.split("/");
   var lastSegment = parts.pop() || parts.pop(); // handle potential trailing slash
