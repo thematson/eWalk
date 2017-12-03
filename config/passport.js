6af023248d42acf187a0e11648190c1efb7941bc
Heather <hlmathies@gmail.com>
2017-12-03 13:10:42 -0500

    cleaned up comments

--- config/passport.js
+++ config/passport.js
@@ -1,39 +1,41 @@
+// Pull in required dependencies
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 var db = require("../models");
+
 passport.use(new LocalStrategy(
-  
-//Our user will log in using an email addressInput
-{
-  usernameField: "userName"
-},
-function(id, password, done){
-  //When a user logs in
-  db.Hotel.findOne({
-    where: {
-      userName: id
-    }
-  }).then(function(dbHotel){
 
-    //If no user with given userName
+  //Our user will log in using an username
+  {
+    usernameField: "userName"
+  },
+  function(id, password, done) {
+    //When a user logs in
+    db.Hotel.findOne({
+      where: {
+        userName: id
+      }
+    }).then(function(dbHotel) {
 
-    if(!dbHotel || !dbHotel.validPassword(password)){
+      //If no user with given userName
 
-      return done(null, false, {
-        message: "Incorrect login."
-      });
-    }
-    //If none of the above, return the user
-    return done(null, dbHotel);
-  });
-}
+      if (!dbHotel || !dbHotel.validPassword(password)) {
+
+        return done(null, false, {
+          message: "Incorrect login."
+        });
+      }
+      //If none of the above, return the user
+      return done(null, dbHotel);
+    });
+  }
 ));
 
-passport.serializeUser(function(hotel, cb){
-  cb (null, hotel);
+passport.serializeUser(function(hotel, cb) {
+  cb(null, hotel);
 });
 
-passport.deserializeUser(function(obj, cb){
+passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
 
