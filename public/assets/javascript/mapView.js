45894fe1bea97ae7709c709ec0a72166e3825c5b
Heather <hlmathies@gmail.com>
2017-12-03 13:58:40 -0500

    format code but code is not currently being used in app

--- public/assets/javascript/mapView.js
+++ public/assets/javascript/mapView.js
@@ -1,11 +1,13 @@
+//~function to display hotels based on zipcode ~~not currently being used but reserved for future versions
 $(document).ready(function() {
-    var db = require("../models");
+  var db = require("../models");
 
-  db.Hotel.findAll({ attributes: ["routingNumber"] }).then(function(result) {
+  db.Hotel.findAll({
+    attributes: ["routingNumber"]
+  }).then(function(result) {
     var markerArray = result.map(function(item) {
       return item.routingNumber;
     });
     console.log(markerArray);
   });
 });
-
